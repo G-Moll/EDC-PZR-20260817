7cfdca962dca7ef7bc1bbd75a9a9200c3beef8e2
RDK <photocromatic@gmail.com>
2026-09-02 13:39:16 -0600

    edc

--- M01-PC/MOODLE/S04-PF/JS-PF-a/js/script.js
+++ M01-PC/MOODLE/S04-PF/JS-PF-a/js/script.js
@@ -2,15 +2,31 @@
 let paper = document.getElementById( "paper" );
 let scissor = document.getElementById( "scissor" );
 let rock = document.getElementById( "rock" );
-// Modules ES6
-// Functions
+
+let matches = document.getElementsByClassName( "match" );
+
+// Event listeners
 paper.addEventListener( "click", optionClickHandler );
 scissor.addEventListener( "click", optionClickHandler );
 rock.addEventListener( "click", optionClickHandler );
 
+matches[ 0 ].addEventListener( "click", matchClickHandler );
+matches[ 1 ].addEventListener( "click", matchClickHandler );
+matches[ 2 ].addEventListener( "click", matchClickHandler );
+matches[ 3 ].addEventListener( "click", matchClickHandler );
+
+
+// Event handlers
 function optionClickHandler( e ) {
     let option = e.target.getAttribute( "data-option" );
     console.log( "Escogiste " + option + "..!" );
 }
+
+function matchClickHandler( e ) {
+    let description = e.target.innerText;
+    console.log( description );
+}
+
+
 
 
